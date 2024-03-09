8b7e3b1616554d3ff1a08196c5bf64d3d8f12c8a
MAHTO ANJALI <159051588+MAHTO-ANJALI@users.noreply.github.com>
2024-03-09 17:40:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TeamInflex/InflexMusicBot",
+    "https://github.com/MAHTO-ANJALI/AnjaliMusicV02",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
